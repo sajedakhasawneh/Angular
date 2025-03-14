214aa373bd7cb8e08ab4b8946a2f5d7a97ccd0bd
sajedakhasawneh <sajedakhasawneh5@gmail.com>
2025-03-14 05:50:58 +0300

    Task2

---
+++ Task2/Task2/task2.client/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CategoryComponent } from './category/category.component';
+import { ProductComponent } from './product/product.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CategoryComponent,
+    ProductComponent,
+    NavBarComponent
+  ],
+  imports: [
+    BrowserModule, HttpClientModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
